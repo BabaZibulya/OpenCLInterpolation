c3e5212d2a796f4334432b333e086f4f5a023910
Mykola Bondarenko <bondarenko_mykola@yahoo.com.ua>
2019-09-14 17:39:10 +0300

    Working MPI

--- InterpolationProblem.cpp
+++ InterpolationProblem.cpp
@@ -35,13 +35,81 @@ void InterpolationProblem::solve()
 		CLLog(platform.getPlatformInfo());
 	}
 
-    if (worldSize == 1)
-    {
+    if (worldSize == 1) {
         OpenCLSubtask subtask({0, Pk}, Lmz, Mmz, Nmz, platforms[0]);
         subtask.solve();
         auto& res = subtask.getResult();
 	    checkResults(res);
+    } else {
+        unsigned nodeChunkSize = Pk / worldSize;
+        if (worldRank == 0) {
+            // Server
+            OpenCLSubtask subtask({0, nodeChunkSize}, Lmz, Mmz, Nmz, platforms[1]);
+            subtask.solve();
+            auto& res = subtask.getResult();
+
+            std::vector<std::future<void>> futures;
+            std::vector<FourDimContiniousArray<float>> results;
+            for (int otherNode = 1; otherNode < worldSize; ++otherNode)
+            {
+                results.emplace_back(res.getDimSizeFirst(),
+                                     res.getDimSizeSecond(),
+                                     res.getDimSizeThird(),
+                                     res.getDimSizeFourth());
+                CLLog("Allocated result buf with ", results.back().getNumberOfElements());
+            }
+            for (int otherNode = 1; otherNode < worldSize; ++otherNode)
+            {
+                auto fut = std::async(std::launch::async,
+                                      [resBuf = results[otherNode - 1].getData(),
+                                       numberOfEl = results[otherNode - 1].getNumberOfElements()](unsigned otherNode) {
+                                          MEASURE_TIME("Time to receive ",
+                                          CLLog("Receiving data from node = ", otherNode,
+                                                " rest buf size = ", numberOfEl);
+                                          MPI_Recv(resBuf,
+                                                   numberOfEl, // All chuncks are same size
+                                                   MPI_FLOAT,
+                                                   otherNode,
+                                                   0,
+                                                   MPI_COMM_WORLD,
+                                                   MPI_STATUS_IGNORE);
+                                          CLLog("Received data from node = ", otherNode);
+                                          );
+                                      },
+                                      otherNode);
+                futures.emplace_back(std::move(fut));
+            }
+
+
+            for (auto& fut : futures) {
+                CLLog("Waiting for future");
+                fut.get();
+            }
+
+            for (auto& res : results) {
+                checkResults(res);
+            }
+            checkResults(res);
+        } else {
+            // Workers
+            unsigned left = nodeChunkSize * worldRank;
+            unsigned right = nodeChunkSize * (worldRank + 1);
+            CLLog("Worker = ", worldRank, " left = ", left, " right = ", right);
+            OpenCLSubtask subtask({left, right},
+                                   Lmz, Mmz, Nmz, platforms[0]);
+            subtask.solve();
+            auto& res = subtask.getResult();
+
+            CLLog("Worker = ", worldRank, " rest buf size = ", res.getNumberOfElements());
+            MPI_Send(res.getData(),
+                     res.getNumberOfElements(),
+                     MPI_FLOAT,
+                     0,
+                     0,
+                     MPI_COMM_WORLD);
+        }
     }
+    
 }
 
 void InterpolationProblem::solve(unsigned chunkSize)
--- TimeUtils.h
+++ TimeUtils.h
@@ -3,4 +3,15 @@
 #include <string>
 #include <functional>
 
+#define MEASURE_TIME(message, FUNC) \
+    { \
+        std::chrono::time_point<std::chrono::system_clock> start, end; \
+        start = std::chrono::system_clock::now(); \
+        FUNC; \
+        end = std::chrono::system_clock::now(); \
+        int elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds> \
+                                (end-start).count(); \
+        std::cout << message << " - execution time: " << elapsed_milliseconds << "ms\n"; \
+    }
+
 void measureTime(const std::string& message, const std::function<void()>& f);
